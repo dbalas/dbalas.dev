1fbcfa37acc3788e91ecc5d25eb589c1762549be
Daniel <dbalastegui@gmail.com>
2019-03-10 12:35:20 +0100

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'dbalas.dev',
+    script: 'yarn start',
+    exec_mode: 'cluster', // enables clustering
+    instances: 'max', // or an integer
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
