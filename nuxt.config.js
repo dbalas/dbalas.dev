22774b79006bb38bc80d3351f254d8e7660b933e
dbalas <dbalasdev@gmail.com>
2019-05-21 21:08:41 +0200

    updating web title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'Daniel Balastegui - Software Engineer',
     meta: [
       {
         charset: 'utf-8'
